1062cbeb422e2ebeb26ebe57639f9c4042b43c68
rain-li <lizlok@gmail.com>
2025-11-18 14:57:34 +0800

    vfs-ui: pre log;

--- packages/vfs-ui/src/components/NodeList/items/BaseNodeItem.ts
+++ packages/vfs-ui/src/components/NodeList/items/BaseNodeItem.ts
@@ -4,20 +4,23 @@
  */
 import { VFSNodeUI } from '../../../types/types';
 
-export interface NodeItemCallbacks {
-    onClick: (id: string, event: MouseEvent) => void;
-    onContextMenu: (id: string, event: MouseEvent) => void;
-}
+// [修正] NodeItemCallbacks 接口不再需要，可以删除
+// export interface NodeItemCallbacks {
+//     onClick: (id: string, event: MouseEvent) => void;
+//     onContextMenu: (id: string, event: MouseEvent) => void;
+// }
 
 export abstract class BaseNodeItem {
     public readonly element: HTMLElement;
     protected readonly item: VFSNodeUI;
-    protected readonly callbacks: NodeItemCallbacks;
+    // [修正] callbacks 属性已移除
+    // protected readonly callbacks: NodeItemCallbacks;
     protected readonly isReadOnly: boolean;
 
-    constructor(item: VFSNodeUI, callbacks: NodeItemCallbacks, isReadOnly: boolean) {
+    // [修正] 构造函数不再接收 callbacks
+    constructor(item: VFSNodeUI, isReadOnly: boolean) {
         this.item = item;
-        this.callbacks = callbacks;
+        // this.callbacks = callbacks;
         this.isReadOnly = isReadOnly;
         this.element = this.createRootElement();
         if (!this.isReadOnly) {
@@ -28,32 +31,11 @@ export abstract class BaseNodeItem {
     protected abstract createRootElement(): HTMLElement;
     public abstract update(props: any): void;
 
-    public bindEvents(): void {
-        this.element.addEventListener('click', this.handleClick);
-        if (!this.isReadOnly) {
-            this.element.addEventListener('contextmenu', this.handleContextMenu);
-        }
-    }
 
-    public unbindEvents(): void {
-        this.element.removeEventListener('click', this.handleClick);
-        if (!this.isReadOnly) {
-            this.element.removeEventListener('contextmenu', this.handleContextMenu);
-        }
-    }
-
-    private handleClick = (event: MouseEvent): void => {
-        this.callbacks.onClick(this.item.id, event);
-    };
-
-    private handleContextMenu = (event: MouseEvent): void => {
-        event.preventDefault();
-        event.stopPropagation();
-        this.callbacks.onContextMenu(this.item.id, event);
-    };
 
     public destroy(): void {
-        this.unbindEvents();
+        // [修正] unbindEvents 不再需要调用
+        // this.unbindEvents();
         this.element.remove();
     }
 }
